98df721d66bbfd9113704976f236ee2d5feabc1a
fangzhiyang <fanmcgrady@gmail.com>
2018-04-27 00:51:08 +0800

    lesson3

---
+++ lesson3/addCgi/addCgi.js
@@ -0,0 +1,29 @@
+var util = require('../util.js')
+
+Page({
+    data: {
+        requestResult: ''
+    },
+
+    testCgi: function () {
+        util.showBusy('请求中...')
+        var self = this
+        wx.request({
+          url: 'http://localhost:8080/hello', // 接口地址
+          data: {
+            studentId: '11',
+            comment: '22'
+          },
+          header: {
+            'content-type': 'application/json' // 默认值
+          },
+          success: function (res) {
+            console.log(res.data)
+            util.showSuccess('请求成功完成')
+            self.setData({
+              requestResult: JSON.stringify(res.data)
+              })
+          }
+        })
+    }
+})
